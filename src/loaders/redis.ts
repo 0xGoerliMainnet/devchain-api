4a21ae6703cbb28686cad6613436461a63a037c4
ruzgarataozkan <devchainnet@gmail.com>
2024-02-07 05:38:15 +0300

    test

--- src/loaders/redis.ts
+++ src/loaders/redis.ts
@@ -34,6 +34,8 @@ async function load_redis(options: any) {
     await client.set('settings', settings);
   }
 
+  await client.set('devchain_test', '1');
+
   options.redis = client;
 
   return client;
